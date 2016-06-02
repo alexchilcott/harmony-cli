51c55815446df06167ef2babdb0a0cbf13f6a6ef
Alex Chilcott <alexchilcott@gmail.com>
2016-06-02 22:24:18 +0100

    unit test for discover function

--- test/discover.js
+++ test/discover.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const assert = chai.assert;
-const should = chai.should();
+const expect = chai.expect;
 const sinon = require('sinon');
 const mockery = require('mockery');
 
@@ -10,34 +10,46 @@ describe('Actions - Discover', function() {
   describe('handler', function() {
     beforeEach(function() {
       mockery.enable();
+      this.clock = sinon.useFakeTimers();
     });
 
     afterEach(function() {
+      this.clock.restore();
       mockery.disable();
     });
 
-    it('should return -1 when the value is not present', function(done) {
-      const foundHubs = {
-        data: 'some hub data'
-      };
-      class MockHarmonyHubDiscoverer extends EventEmitter {
-        start() {
-          this.emit('online', foundHubs);
+    it('should return the array of hubs found by the harmony hub ' +
+      'discover module',
+      function(done) {
+        // arrange
+        const foundHub1 = {
+          data: 'some hub data'
+        };
+        const foundHub2 = {
+          data: 'some more hub data'
+        };
+        class MockHarmonyHubDiscoverer extends EventEmitter {
+          start() {
+            this.emit('online', foundHub1);
+            this.emit('online', foundHub2);
+          }
+          stop() {}
         }
-      }
+        mockery.registerMock('harmonyhubjs-discover',
+          MockHarmonyHubDiscoverer);
 
-      mockery.registerMock('harmonyhubjs-discover',
-        MockHarmonyHubDiscoverer);
+        // act
+        const discoverHandler = require(
+          '../src/commands/handlers/discover');
+        const timeout = 10000;
+        const promise = discoverHandler(timeout)
+        this.clock.tick(timeout);
 
-      const discoverCmd = require('../src/commands/discover');
-      discoverCmd.handler({
-        timeout: 1000
-      })
-
-
-
-      assert.equal(-1, [1, 2, 3].indexOf(5));
-      assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
+        // assert
+        promise.then(x => {
+          expect(x).to.deep.equal([foundHub1, foundHub2]);
+          done();
+        });
+      });
   });
 });
